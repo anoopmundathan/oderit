d2588fd07daf94d824601d78b86a7a04d5687ee4
Anoop Mundathan <anoop.mundathan@gmail.com>
2018-01-25 15:18:50 +0000

    Add live url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,5 @@
-const apiUrl = 'http://localhost:3000/api/v1'
+// const apiUrl = 'http://localhost:3000/api/v1'
+const apiUrl = 'https://shrouded-caverns-78817.herokuapp.com/api/v1'
 
 export const fetchStores = async () => {
   try {
