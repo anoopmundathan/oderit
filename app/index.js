21b361b1f21bb6b06b6e7a3d26d42c66cd54fdb4
Anoop Mundathan <anoop.mundathan@gmail.com>
2018-01-26 15:04:37 +0000

    Add config folder

--- app/index.js
+++ app/index.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
 import { red } from './utils/colors'
-import { MainNavigator } from './components/Navigator'
+import { MainNavigator } from './config/Navigator'
 import { AppStatusBar } from './components/AppStatusBar'
 
 import { Provider } from 'react-redux'
-import store from './store'
+import store from './config/store'
 
 export default class App extends Component {
   render() {
